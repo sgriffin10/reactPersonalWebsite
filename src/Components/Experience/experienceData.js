849e455cd2ecdef2268cbb787f49f213419e13eb
sgriffin10 <sgriffin4@babson.edu>
2023-08-23 10:19:45 +0800

    update logos; update technologies to list

--- src/Components/Experience/experienceData.js
+++ src/Components/Experience/experienceData.js
@@ -1,10 +1,10 @@
 // experienceData.js
 export const experienceData = [
   {
-    title: "Chief Technology Officer (Part-Time)",
+    title: "Chief Technology Officer",
     company: "Spur",
-    dates: "March 2023 – Present",
-    companyLogo: "path/to/spur-logo.png",
+    dates: "March 2023 – Present (Part-Time)",
+    companyLogo: "Logos/spurgb.png",
     description: [
       "Lead the development and implementation of Spur, a social media app designed to deepen friendships through spontaneous interactions, managing stakeholder expectations while aligning business and user needs",
       "Use Express.js to manage a RESTful API, streamlining interactions with MongoDB for complex data handling, managing friend-group relationships, and bolstering front-end application integration",
@@ -12,32 +12,38 @@ export const experienceData = [
       "Employ Firebase for secure user authentication, creating user persistence, and strategically passing authentication data to the MongoDB back-end",
       "Utilize Flutter and Dart for front-end development, establishing an MVC architecture for state and code management",
     ],
-    technologies:
-      "Express.js, MongoDB, Google Cloud Platform, Firebase, Flutter, Dart",
+    technologies: [
+      "Express.js",
+      "MongoDB",
+      "Google Cloud Platform",
+      "Firebase",
+      "Flutter",
+      "Dart",
+    ],
   },
   {
     title: "E-Commerce Associate",
     company: "Lowe-Martin Group",
-    dates: "September 2022 – Present",
-    companyLogo: "path/to/lowe-martin-logo.png",
+    dates: "September 2022 – Present (Full-Time)",
+    companyLogo: "Logos/lmg.jpeg",
     description: [
       "Lead the development and maintenance of client fulfillment websites, employing HTML and CSS for tailored UI enhancements and integrating additional data collection fields",
       "Engineer automation processes using Python and Selenium, effectively expediting user onboarding, product overhauls, and report generation, resulting in reduced manual effort and increased operational efficiency",
       "Manage site content and user access, implementing seamless addition of products and users based on client requests, showcasing problem-solving capabilities and a commitment to customer service",
     ],
-    technologies: "HTML, CSS, Python, Selenium",
+    technologies: ["HTML", "CSS", "Python", "Selenium"],
   },
   {
     title: "Software Engineering Intern",
     company: "FastSpring",
     dates: "September 2021 – September 2022",
-    companyLogo: "path/to/fastspring-logo.png",
+    companyLogo: "Logos/fs.jpeg",
     description: [
       "Resolved high-priority customer issues relating to global online payments, subscription management, branded checkout, and global taxes, resulting in a 40% reduction in customer-facing software bugs",
       "Employed log4j, Looker, and Sumo Logic for custom logging and advanced log analysis, identifying and resolving issues with failing Dutch bank processing orders, saving customers upwards of $10K in lost revenue",
       "Expanded the wire transfer payment service using Java and DynamoDB to enable customers in over 25 previously unsupported countries to securely and efficiently complete their transactions",
     ],
-    technologies: "log4j, Looker, Sumo Logic, Java, DynamoDB",
+    technologies: ["log4j", "Looker", "Sumo Logic", "Java", "DynamoDB"],
   },
   // You can add the "PROJECTS" section as another object or handle it separately
 ];
