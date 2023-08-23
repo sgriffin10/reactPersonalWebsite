ded53acb95e985b56819d8048821696efe9cfbb8
sgriffin10 <sgriffin4@babson.edu>
2023-08-23 10:16:58 +0800

    add experience and about sections

--- src/Landing.js
+++ src/Landing.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Header from "./Components/Header";
 import Introduction from "./Components/Introduction";
+import Experience from "./Components/Experience/Experience";
+import About from "./Components/About/About";
 
 function Landing() {
   return (
-    <div className="full-viewport-section landing">
-      <Header />
-      <Introduction />
+    <div>
+      <div className="full-viewport-section landing">
+        <Header />
+        <Introduction />
+      </div>
+      <div className="full-viewport-section">
+        <Experience />
+      </div>
+      {/* <div className="full-viewport-section landing"> */}
+      <div>
+        <About />
+      </div>
     </div>
   );
 }
